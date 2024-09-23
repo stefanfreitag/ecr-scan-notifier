5235c7bf1c87add59acf3bef1192af97171d8a85
Stefan Freitag <stefan.freitag@rwe.com>
2024-09-23 19:23:37 +0200

    chore: upgrade cdk dependencies

--- .projenrc.js
+++ .projenrc.js
@@ -6,7 +6,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorName: 'Stefan Freitag',
   authorEmail: 'stefan.freitag@udo.edu',
   authorOrganization: false,
-  cdkVersion: '2.156.0',
+  cdkVersion: '2.159.1',
   //majorVersion: 1,
   defaultReleaseBranch: 'main',
   description: 'Notifies on new AWS ECR scan results',
